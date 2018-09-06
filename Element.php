a3773d6d540bf21a7d1b271f33b0f6e3c24d6570
EvgenijK <komov.evgen@gmail.com>
2018-09-06 13:35:40 +0300

    Added \ArrayAcces implementation to Element and ElementCollection classes

--- Element.php
+++ Element.php
@@ -2,7 +2,7 @@
 
 namespace htmlOOP;
 
-class Element
+class Element implements \ArrayAccess
 {
 	/**
 	 * @var string name
@@ -48,12 +48,29 @@ class Element
 		$this->name = $value;
 	}
 
+	/**
+	 * @param Element $element
+	 */
 	public function append(Element $element)
 	{
-		$this->children->add_element($element);
+		$this->children[] = $element;
 		$element->setParent($this);
 	}
 
+	/**
+	 * @param Element $element
+	 * @param int $offset
+	 */
+	public function setChild(int $offset, Element $element)
+	{
+		$this->children[$offset] = $element;
+		$element->setParent($this);
+	}
+
+	/**
+	 * @param string $attribute
+	 * @param $value
+	 */
 	public function setAttribute(string $attribute, $value)
 	{
 		$this->attributes[$attribute] = (string) $value;
@@ -67,4 +84,69 @@ class Element
 		$this->parent = $parent;
 	}
 
+	/**
+	 * Whether a offset exists
+	 * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+	 * @param mixed $offset <p>
+	 * An offset to check for.
+	 * </p>
+	 * @return boolean true on success or false on failure.
+	 * </p>
+	 * <p>
+	 * The return value will be casted to boolean if non-boolean was returned.
+	 * @since 5.0.0
+	 */
+	public function offsetExists($offset)
+	{
+		return isset($this->children[$offset]);
+	}
+
+	/**
+	 * Offset to retrieve
+	 * @link https://php.net/manual/en/arrayaccess.offsetget.php
+	 * @param mixed $offset <p>
+	 * The offset to retrieve.
+	 * </p>
+	 * @return mixed Can return all value types.
+	 * @since 5.0.0
+	 */
+	public function offsetGet($offset)
+	{
+		return isset($this->children[$offset]) ? $this->children[$offset] : null;
+	}
+
+	/**
+	 * Offset to set
+	 * @link https://php.net/manual/en/arrayaccess.offsetset.php
+	 * @param mixed $offset <p>
+	 * The offset to assign the value to.
+	 * </p>
+	 * @param mixed $value <p>
+	 * The value to set.
+	 * </p>
+	 * @return void
+	 * @since 5.0.0
+	 */
+	public function offsetSet($offset, $value)
+	{
+		if (is_null($offset)) {
+			$this->append($value);
+		} else {
+			$this->setChild($offset, $value);
+		}
+	}
+
+	/**
+	 * Offset to unset
+	 * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+	 * @param mixed $offset <p>
+	 * The offset to unset.
+	 * </p>
+	 * @return void
+	 * @since 5.0.0
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->children[$offset]);
+	}
 }
--- ElementCollection.php
+++ ElementCollection.php
@@ -9,29 +9,33 @@
 namespace htmlOOP;
 
 
-class ElementCollection
+class ElementCollection implements \ArrayAccess
 {
 
 	/**
 	 * @var array elements
 	 */
-	private $elements;
+	protected $elements;
 
+	/**
+	 * @return array
+	 */
 	public function get_elements()
 	{
 		return $this->elements;
 	}
 
-	public function add_element(Element $element)
-	{
-		$this->elements[] = $element;
-	}
-
+	/**
+	 * @param ElementCollection $collection
+	 */
 	public function merge_collection(ElementCollection $collection)
 	{
 		array_merge($this->elements, $collection->get_elements());
 	}
 
+	/**
+	 * @param $callback
+	 */
 	public function each($callback)
 	{
 		if (!is_callable($callback))
@@ -45,4 +49,72 @@ class ElementCollection
 		}
 	}
 
+	/**
+	 * Whether a offset exists
+	 * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+	 * @param mixed $offset <p>
+	 * An offset to check for.
+	 * </p>
+	 * @return boolean true on success or false on failure.
+	 * </p>
+	 * <p>
+	 * The return value will be casted to boolean if non-boolean was returned.
+	 * @since 5.0.0
+	 */
+	public function offsetExists($offset)
+	{
+		return isset($this->elements[$offset]);
+	}
+
+	/**
+	 * Offset to retrieve
+	 * @link https://php.net/manual/en/arrayaccess.offsetget.php
+	 * @param mixed $offset <p>
+	 * The offset to retrieve.
+	 * </p>
+	 * @return mixed Can return all value types.
+	 * @since 5.0.0
+	 */
+	public function offsetGet($offset)
+	{
+		return isset($this->elements[$offset]) ? $this->elements[$offset] : null;
+	}
+
+	/**
+	 * Offset to set
+	 * @link https://php.net/manual/en/arrayaccess.offsetset.php
+	 * @param mixed $offset <p>
+	 * The offset to assign the value to.
+	 * </p>
+	 * @param mixed $value <p>
+	 * The value to set.
+	 * </p>
+	 * @return void
+	 * @since 5.0.0
+	 */
+	public function offsetSet($offset, $value)
+	{
+		if (is_null($offset)) {
+			$this->elements[] = $value;
+		} else if(is_int($offset)) {
+			$this->elements[$offset] = $value;
+		} else {
+			// only numeric offset allowed in ElementsCollection
+			// todo throw exception
+		}
+	}
+
+	/**
+	 * Offset to unset
+	 * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+	 * @param mixed $offset <p>
+	 * The offset to unset.
+	 * </p>
+	 * @return void
+	 * @since 5.0.0
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->elements[$offset]);
+	}
 }
