6754f6d0b01f612cb2fe2db040be4682f546b162
Evgenij Komov <komov.evgen@gmail.com>
2018-09-10 08:37:20 +0300

    Some work on html elements

--- Html/HtmlElement/HtmlElement.php
+++ Html/HtmlElement/HtmlElement.php
@@ -9,105 +9,177 @@
 namespace htmlOOP\Html\HtmlElement;
 
 use htmlOOP\Element\Element;
+use htmlOOP\Html\IndexedCollection\HtmlElementCollection;
 
 class HtmlElement extends Element
 {
 
-    /**
-     * @var string index
-     */
-    private $index;
+	const SPECIAL_DATA_NAME = 'name';
+	const SPECIAL_DATA_ID = 'id';
 
-    /**
-     * @var ElementCollection indexed_elements
-     */
-    private $indexed_elements;
+	/**
+	 * Root element of the structure
+	 *
+	 * @var HtmlElement $root
+	 */
+	protected $root;
 
-    /**
-     * Element constructor.
-     *
-     * @param Element|string $values
-     */
-    public function __construct(...$values)
-    {
-        $tmp_children = new ElementCollection();
-        $tmp_attributes = [];
+	/**
+	 * Unique id in the structure
+	 *
+	 * @var string $id
+	 */
+	protected $id;
 
-        foreach ($values as $index => $value)
-        {
-            if ($value instanceof Element)
-            {
+	/**
+	 * @var HtmlElementCollection $indexed_elements
+	 */
+	protected $indexed_elements;
 
-                if (is_string($index) && $this->check_index($index))
-                {
-                    $value->set_index($index);
-                }
+	/**
+	 * @var string[] $specialData
+	 */
+	protected $specialData = [
+	];
 
-                $tmp_children[] = $value;
-            } elseif ($value instanceof ElementCollection)
-            {
-                $tmp_children->merge_collection($value);
-            } elseif (is_string($value) || is_numeric($value))
-            {
-                if (is_string($index))
-                {
-                    // добавляется аттрибут
+	/**
+	 * HtmlElement constructor.
+	 *
+	 * @param array       $data
+	 * @param HtmlElement ...$children
+	 */
+	public function __construct(array $data = [], HtmlElement ...$children)
+	{
+		$this->specialData[] = HtmlElement::SPECIAL_DATA_ID;
+		$this->specialData[] = HtmlElement::SPECIAL_DATA_NAME;
 
-                    if (key_exists($index, $this->attributes))
-                    {
-                        // todo throw exception
-                    }
+		$this->root = $this;
 
-                    $tmp_attributes[$index] = $value;
+		parent::__construct($data, $children);
+	}
 
-                } elseif (is_numeric($index) && (is_string($value) || is_numeric($value)))
-                {
-                    // добавляется просто текст в значение элемента
-                    $tmp_children[] = new Element($value);
-                } else
-                {
-                    // todo throw exception
-                }
-            }
-        }
+	/**
+	 * @param        $index
+	 * @param string $value
+	 */
+	public function setData($index, string $value)
+	{
+		if (!$this->setSpecialData($index, $value))
+		{
+			$this->data[$index] = $value;
+		}
+	}
 
-        foreach ($tmp_children->get_elements() as $element)
-        {
-            $this->add_child($element);
-        }
+	/**
+	 * @param        $index
+	 * @param string $value
+	 *
+	 * @return bool
+	 */
+	protected function setSpecialData($index, string $value)
+	{
+		if (in_array($index, $this->specialData))
+		{
+			$this->$index = $value;
 
-        foreach ($tmp_attributes as $attribute => $value)
-        {
-            $this->set_attribute($attribute, $value);
-        }
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+
+	/**
+	 * @param HtmlElement      $root
+	 * @param HtmlElement|NULL $oldRoot
+	 */
+	protected function setRoot(HtmlElement $root, $oldRoot = NULL)
+	{
+		if ($oldRoot === NULL && $this->root === $this)
+		{
+			foreach ($this->children as $child)
+			{
+				$child->setRoot($root, $this);
+			}
+
+			$this->root = $root;
+
+		} elseif ($oldRoot instanceof HtmlElement)
+		{
+			if ($this->root === $oldRoot)
+			{
+				foreach ($this->children as $child)
+				{
+					$child->setRoot($root, $oldRoot);
+				}
+
+				$this->root = $root;
+			}
+		}
+	}
+
+	/**
+	 * @return HtmlElement
+	 */
+	public function getRoot()
+	{
+		return $this->root;
+	}
+
+	/**
+	 * @param HtmlElement $parent
+	 */
+	protected function setParent(HtmlElement $parent)
+	{
+		$this->setRoot($parent->getRoot());
+
+		parent::setParent($parent);
+	}
 
 
-    }
+	/////////////////////////////////////////
 
-    public function set_index(string $value)
-    {
-        // todo добавить проверку
-        if (!$this->check_index($value))
-        {
-            // todo throw exception
-        }
 
-        $this->index = $value;
-        $this->indexed_elements->add_element($this);
-    }
+	public function set_index(string $value)
+	{
+		// todo добавить проверку
+		if (!$this->check_index($value))
+		{
+			// todo throw exception
+		}
 
-    public function get_index()
-    {
-        return $this->index;
-    }
+		$this->index = $value;
+		$this->indexed_elements->add_element($this);
+	}
 
-    /**
-     * @param string $index
-     *
-     * @return bool
-     */
-    private function check_index(string $index)
-    {
-        return TRUE;
-    }
+	/**
+	 * @param string $attribute
+	 * @param        $value - Will be converted to a string by (string)
+	 */
+	public function setAttribute(string $attribute, $value)
+	{
+		$this->data[$attribute] = (string) $value;
+	}
+
+	/**
+	 * @param string $value
+	 */
+	public function setName(string $value)
+	{
+		$this->name = $value;
+	}
+
+	public function get_index()
+	{
+		return $this->index;
+	}
+
+	/**
+	 * @param string $index
+	 *
+	 * @return bool
+	 */
+	protected function check_index(string $index)
+	{
+		return TRUE;
+	}
 }
