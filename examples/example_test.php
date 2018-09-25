75e1e33b5da0468f15dd823dbcaf80d3e8cedb33
EvgenijK <komov.evgen@gmail.com>
2018-09-25 11:21:43 +0300

    Tests folder renamed to 'examples'

--- examples/example_test.php
+++ examples/example_test.php
@@ -30,18 +30,27 @@ try
 
 	$test_element_2 = new HtmlElement(
 		['tag' => 'div','id' => 'old_root'],
-		new HtmlElement(['tag' => 'div','name' => 'old_el_1']),
+		new HtmlElement(['tag' => 'div','id' => 'old_el_1']),
 		new HtmlElement(['tag' => 'div','id' => 'old_el_2']),
 		new HtmlElement(['tag' => 'div','id' => 'old_el_3'])
 	);
 
-	$test_element[1][2]->addChildren($test_element_2);
-	var_dump($test_element_2[1]->getRoot()->getIndexKeys());
-	var_dump($test_element_2[1]->getIndex()['inner_1_2_2']->getId());
-	var_dump($test_element->getById('old_el_3')->getParent()->getId());
-
+	$test_element[0]->addChildren($test_element_2);
+//	var_dump($test_element_2[1]->getRoot()->getIndexKeys());
+//	var_dump($test_element_2[1]->getIndex()['inner_1_2_2']->getId());
+//	var_dump($test_element->getById('old_el_3')->getParent()->getId());
+//
 	$test_element->htmlRender();
 
+	$fired_tree = $test_element_2->unsetElement(false);
+	$test_element->htmlRender();
+
+	foreach ($test_element[1][2]->getChildren() as $item)
+	{
+		echo $test_element[1][2]->getId() . ' child: ' . $item->getId() . PHP_EOL;
+	};
+
+//    var_dump($fired_tree);
 
 } catch (Exception $e)
 {
